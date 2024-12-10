955c59094a1e65bfd4be68419e6ee2ab93b360d1
Krishnan Shankar <krishnans2006@gmail.com>
2024-12-10 00:47:35 -0600

    Finish day 9 part 1

--- day9.py
+++ day9.py
@@ -1,32 +1,33 @@
-import tqdm
-import tqdm.contrib
-
-with open("day9test.txt", "r") as file:
+with open("day9.txt", "r") as file:
     filesystem_input = "".join([line.strip() for line in file])
 
-filesystem = ""
+filesystem = []
 current_file_id = 0
 for i, char in enumerate(filesystem_input):
     if i % 2 == 0:  # file
-        filesystem += str(current_file_id) * int(char)
+        filesystem.extend([current_file_id] * int(char))
         current_file_id += 1
     else:  # free space
-        filesystem += "." * int(char)
+        filesystem.extend([-1] * int(char))
 
-final_file_id = 0
-for i in tqdm.trange(len(filesystem)):
-    if len(set(filesystem[i:])) == 1:  # only free space left
-        final_file_id = i - 1
-        break
 
-    if filesystem[i] == ".":
-        for j in range(len(filesystem) - 1, -1, -1):
-            if filesystem[j] != ".":
-                filesystem = filesystem[:i] + filesystem[j] + filesystem[i + 1:j] + "." + filesystem[j + 1:]
-                break
+pointer_left = 0
+pointer_right = len(filesystem) - 1
+
+while pointer_left < pointer_right:
+    if filesystem[pointer_left] != -1:
+        pointer_left += 1
+        continue
+    if filesystem[pointer_right] == -1:
+        pointer_right -= 1
+        continue
+
+    filesystem[pointer_left], filesystem[pointer_right] = filesystem[pointer_right], filesystem[pointer_left]
 
 checksum = 0
-for i, file in tqdm.contrib.tenumerate(filesystem[:final_file_id + 1]):
-    checksum += i * int(file)
+for i, file in enumerate(filesystem):
+    if file == -1:
+        break
+    checksum += i * file
 
-print(checksum, filesystem)
+print(checksum)
